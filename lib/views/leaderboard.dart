28c2e056d1eaa8f8454538bf870834cf98bdb97a
Mohab Magdy <mohab_31_8@hotmail.com>
2019-05-05 06:06:46 +0200

    basic implementation of leaderboard

--- lib/views/leaderboard.dart
+++ lib/views/leaderboard.dart
@@ -75,61 +75,78 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
                                     Padding(
                                       padding: const EdgeInsets.only(
                                           top: 0.0),
-                                      child: Column(
+                                      child: Row(
                                         mainAxisAlignment: MainAxisAlignment.start,
                                         crossAxisAlignment: CrossAxisAlignment.start,
                                         children: <Widget>[
 
 
 
-                                              new Container(
-                                                width: 100.0,
-                                                height: 100.0,
-                                                decoration: new BoxDecoration(
-                                                  color: const Color(
-                                                      0xff7c94b6),
-                                                  image: new DecorationImage(
+                                              Column(
 
-                                                    image:
-                                                    new NetworkImage(
-                                                        'http://i.imgur.com/QSev0hg.jpg'),
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                  borderRadius:
-                                                  new BorderRadius.all(
-                                                      new Radius
-                                                          .circular(
-                                                          50.0)),
-                                                  border: new Border.all(
-                                                    color: Colors.white,
-                                                    width: 4.0,
+                                      children:<Widget>[
+
+
+                                           new Container(
+                                                  width: 100.0,
+                                                  height: 100.0,
+                                                  decoration: new BoxDecoration(
+                                                    color: const Color(
+                                                        0xff7c94b6),
+                                                    image: new DecorationImage(
+
+                                                      image:
+                                                      new NetworkImage(
+                                                          'http://i.imgur.com/QSev0hg.jpg'),
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                    borderRadius:
+                                                    new BorderRadius.all(
+                                                        new Radius
+                                                            .circular(
+                                                            50.0)),
+                                                    border: new Border.all(
+                                                      color: Colors.white,
+                                                      width: 4.0,
+                                                    ),
                                                   ),
                                                 ),
+                                              Padding(
+                                                padding: const EdgeInsets.only(top:8.0),
+                                                child: new Text(driverrate),
                                               ),
-                                              new Text(driverrate),
-
-
-                                          Padding(
-                                            padding: const EdgeInsets
-                                                .only(
-                                                bottom:30.0 ,
-                                                left: 7.0),
-
-
-                                            child: Center(
-                                              child: new Text(
-                                                name,
-                                                style: new TextStyle(
-                                                  fontWeight: FontWeight
-                                                      .bold,
-                                                  fontSize: 23.0,
-                                                  color: Colors
-                                                      .black,
-                                                ),
+                                        ],
                                               ),
-                                            ),
-                                          ),
 
+
+
+                                              Column(
+                                                children: <Widget>[
+                                                  Padding(
+                                                    padding: const EdgeInsets
+                                                        .only(
+                                                        bottom:30.0 ,
+                                                        left: 7.0),
+
+
+                                                    child: Center(
+                                                      child: new Text(
+                                                        name,
+                                                        style: new TextStyle(
+                                                          fontWeight: FontWeight
+                                                              .bold,
+                                                          fontSize: 23.0,
+                                                          color: Colors
+                                                              .black,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+
+                                                  Center(child: new Text(numberofridesdriver.toString()+" "+ "rides given")),
+                                                  Center(child: new Text(co2driver+" " +"KG C02 saved")),
+                                                ],
+                                              ),
 //         :new Text("a7a"),
                                         ],
                                       ),
@@ -137,12 +154,7 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
 //                                  ],
 //
 //                                    ),
-                                    subtitle: Column(
-                                      children: <Widget>[
-                                        new Text(numberofridesdriver.toString()+ "rides given"),
-                                        new Text(co2driver+"KG C02 saved"),
-                                      ],
-                                    ),
+
 
                                   ),
 
@@ -169,42 +181,151 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
               //this one is for buds
               StreamBuilder<QuerySnapshot>(
                 stream: Firestore.instance.collection('users')
-                    .where("passengerrate", isGreaterThan: "1").orderBy('passengerrate', descending: true).limit(10).snapshots(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    .where("passengerrate", isGreaterThan: "1").orderBy('passengerrate', descending: true).limit(10).snapshots(),                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+
+//                  String name =
+//                  snapshot.data.documents[0].data['First Name '];
                   if (snapshot.hasError)
                     return new Text('Error: ${snapshot.error}');
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting: return new Text('Loading...');
                     default:
-                      return       new ListView.builder(
-                          itemExtent: 220.0,
+                      return ListView.builder(
+                          itemExtent: 150.0,
                           itemCount: snapshot.data.documents.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            String email =
-                            snapshot.data.documents[index].data['email'].toString();
-                            String name =
-                            snapshot.data.documents[index].data['First Name '].toString();
-                            return Column(
-                              children: <Widget>[
-                                new Image.network(
-                                  "http://i.imgur.com/QSev0hg.jpg",
-                                  fit: BoxFit.fill,
-                                  height: 201.0,
-                                  width: double.infinity,
-                                ),
-                                new Text(
-                                  email,
-                                ),
 
-//                                new Text(
-//                                  email,
-//                                ),
-                              ],
+                          itemBuilder: (BuildContext context, int index) {
+                            String name =
+                            snapshot.data.documents[index].data['First Name '];
+                            int numberofridesdriver = snapshot.data.documents[index].data['Number Of Rides As Driver'];
+                            String CO2passenger = snapshot.data.documents[index].data['CO2passenger'];
+                            String passengerrate = snapshot.data.documents[index].data['passengerrate'];
+
+
+
+
+
+                            return SingleChildScrollView(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(padding: EdgeInsets.all(16.0),
+                                    child: Container(
+                                      child: Material(
+                                        color: Colors.white,
+                                        elevation: 14.0,
+                                        borderRadius: BorderRadius.circular(18.0),
+                                        shadowColor: Color(0x802196F3),
+                                        child:ListTile(
+                                          title:
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                top: 0.0),
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: <Widget>[
+
+
+
+                                                Column(
+
+                                                  children:<Widget>[
+
+
+                                                    new Container(
+                                                      width: 100.0,
+                                                      height: 100.0,
+                                                      decoration: new BoxDecoration(
+                                                        color: const Color(
+                                                            0xff7c94b6),
+                                                        image: new DecorationImage(
+
+                                                          image:
+                                                          new NetworkImage(
+                                                              'http://i.imgur.com/QSev0hg.jpg'),
+                                                          fit: BoxFit.cover,
+                                                        ),
+                                                        borderRadius:
+                                                        new BorderRadius.all(
+                                                            new Radius
+                                                                .circular(
+                                                                50.0)),
+                                                        border: new Border.all(
+                                                          color: Colors.white,
+                                                          width: 4.0,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets.only(top:8.0),
+                                                      child: new Text(passengerrate),
+                                                    ),
+                                                  ],
+                                                ),
+
+
+
+                                                Column(
+                                                  children: <Widget>[
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                          .only(
+                                                          bottom:30.0 ,
+                                                          left: 7.0),
+
+
+                                                      child: Center(
+                                                        child: new Text(
+                                                          name,
+                                                          style: new TextStyle(
+                                                            fontWeight: FontWeight
+                                                                .bold,
+                                                            fontSize: 23.0,
+                                                            color: Colors
+                                                                .black,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+
+                                                    Center(child: new Text(numberofridesdriver.toString()+" "+ "rides given")),
+                                                    Center(child: new Text(CO2passenger+" " +"KG C02 saved")),
+                                                  ],
+                                                ),
+//         :new Text("a7a"),
+                                              ],
+                                            ),
+                                          ),
+//                                  ],
+//
+//                                    ),
+
+
+                                        ),
+
+
+
+
+
+                                      ),
+                                    ),
+
+                                  ),
+
+
+                                ],
+
+                              ),
                             );
-                          });
+
+                          }
+                      );
                   }
                 },
-              ),//              Icon(Icons.directions_bike),
+              ),////              Icon(Icons.directions_bike),
             ],
           ),
         ),
